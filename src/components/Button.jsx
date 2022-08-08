e62fdf11a5d559883ca3a990b8fdaa7f06771be2
miriamcg <contact@artemiscg.dev>
2022-08-08 18:22:44 +0100

    Button Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, StyleSheet, Alert, TouchableOpacity } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+import StyledText from './StyledText.jsx'
+import theme from '../theme.js'
+
+
+const Button = ({title, navigation}) => (
+    <View>
+        <LinearGradient start={styles.linearGradient.startPos} end={styles.linearGradient.endPos} colors={theme.gradients.purpleGradient} 
+        style={title === 'CONTINUE' ? styles.linearGradient : styles.startButton}>
+        <TouchableOpacity onPress={() => navigation.navigate('GroundingExercise')}>
+            <StyledText align='center' colorPrimaryDark='primaryDark' font='font' fontSize='heading'>{title}</StyledText>
+        </TouchableOpacity>
+        </LinearGradient>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    linearGradient: {
+        startPos: [0.35, 0],
+        endPos: [0, 0],
+        borderRadius: 40,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 20,
+        marginHorizontal: 70,
+        paddingTop: 15,
+        paddingBottom: 15,
+        bottom: -75
+    },
+    startButton: {
+        borderRadius: 40,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 20,
+        marginHorizontal: 70,
+        paddingTop: 15,
+        paddingBottom: 15,
+        bottom: -35
+    }
+})
+
+export default Button
+
